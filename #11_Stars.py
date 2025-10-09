956344d029233f891220d5846431b8d77ccffa9b
Rudra <rudragoswami4331@gmail.com>
2025-10-09 09:45:11 +0530

    Easy

---
+++ #11_Stars.py
@@ -0,0 +1,10 @@
+#  Write a program that reads a word and prints the word in "* * * word * * *" format."
+'''
+input: Hello
+output: * * * Hello * * *
+'''
+
+a = ("* * * ")
+b = input()
+c = (a + b + " * * *")
+print(c)
